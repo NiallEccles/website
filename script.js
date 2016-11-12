5dc5e8d5dbafd93c063d60dba0a6f05b5aa9271b
NiallEccles <n.eccles14@gmail.com>
2016-11-12 12:35:31 +0000

    Added in random h1 colour change on hover
    To remove, delete javascript function h1 hover and css a.*color*hover.
    Remove commented out header hover colour.

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ $(document).ready(function(){
    $('li').hide().fadeIn();
   });
 
+  $("h1").hover(function(e)
+      {
+          var randomClass = getRandomClass();
+          $(e.target).attr("class", randomClass);
+      });
+      function getRandomClass()
+      {
+          //Store available css classes
+          var classes = new Array("pink", "blue", "yellow", "lime", "orange", "lilac", "pale_yellow");
+
+          //Get a random number from 0 to 4
+          var randomNumber = Math.floor(Math.random()*6);
+
+          return classes[randomNumber];
+      }
+
+
+
+
 
   $('#icons h2 a i.fa').each(function(i){
   setTimeout(function(){
