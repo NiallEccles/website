fc0b67aabc8fa47145d60fbf6bf11b8e2c6c3b3b
NiallEccles <n.eccles14@gmail.com>
2016-11-12 00:54:06 +0000

    Added in nav elements and more style changes
    Added in three nav elements, about, work and experience. Added a fade in
    effect to nav elements. Changed media queries on social icons so they
    always appear on one line beside each other.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
    $(function() {
    $( "h1" ).hide().fadeIn();
+   $('li').hide().fadeIn();
   });
+
 
   $('#icons h2 a i.fa').each(function(i){
   setTimeout(function(){
     $('#icons h2 a i.fa').eq(i).addClass('is-visible');
   }, 200 * i);
 });
+
+
 
 
 
